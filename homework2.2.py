cf31bcfd042df87f32b8edb4809a52236db5b08c
valeriyaandreevna <b.valeriya.andreevna@gmail.com>
2024-08-03 21:36:57 +0300

    Add files via upload

--- homework2.2.py
+++ homework2.2.py
@@ -1,9 +1,13 @@
-while True:
-    my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-    print(my_list)
-    number = int(input('введите число из списка выше: '))
-    if number >= 0:
-        print('good')
-    else:
-        print('не правильно набран номер...')
-        continue
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+index = 0
+
+while index < len(my_list):
+
+    if my_list[index] < 0:
+        break
+
+    if my_list[index] > 0:
+        print(my_list[index])
+
+    index += 1
